b491d4cf7912623acfbf9a2a504dd0a1fad3386c
Martin Eriksson <martin@martinheterjag.com>
2023-12-12 22:05:44 +0100

    first commit

---
+++ timeinfo.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
+from pyfiglet import figlet_format
+
+
+def print_theme_of_the_day():
+    # URL of the website
+    url = "https://temadagar.se/"
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+    # Check if the request was successful (status code 200)
+    if response.status_code != 200:
+        print("Failed to retrieve the page. Status code:", response.status_code)
+        return
+
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the specific div containing the strings
+    content_div = soup.find('div', id='content', class_='post-single-content box mark-links')
+
+    # Check if the content_div is found
+    if content_div:
+        center_tag = content_div.find('center')
+        a_tags = center_tag.find_all('a')
+
+        # Process and print the text within each a tag
+        for a_tag in a_tags:
+            string_text = a_tag.get_text(strip=True)
+            print(string_text)
+
+
+def print_weather():
+    # url_weather = 'https://wttr.in/Norrkoping?format=3'
+    url_weather = 'https://wttr.in/Norrkoping?Q&m0&lang=sv'
+    try:
+        data = requests.get(url_weather)
+        weather = data.text
+    except:
+        weather = "Hittar ej väder"
+    print(weather.strip())
+
+
+print_weather()
+time = datetime.now().strftime("%H:%M")
+print(figlet_format(time, font='doom'))
+print(date.today().strftime("%d %b %Y"))
+print('')
+print('Dagens temadagar:')
+print_theme_of_the_day()
+
